6ee15500bd8566befe51f7f9f10bf8f029899171
Lawand Dalatieh <lawand87@gmail.com>
2026-02-03 00:39:52 +0100

    add empty test and use the index.html page as content

--- tests/transfer.spec.ts
+++ tests/transfer.spec.ts
@@ -1,2 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { readFileSync } from 'fs';
+
+const html = readFileSync(new URL('../index.html', import.meta.url), 'utf-8');
+
+test.describe('Task 1 - API Mocking (Money Transfer)', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.setContent(html);
+  });
+
+});
